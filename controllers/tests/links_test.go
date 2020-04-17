0620338dc912187087d239c94bbd63c2f338b1b8
Mohammad Torkashvand <m.torkashvand2009@gmail.com>
2020-04-17 13:03:50 +0430

    add a test case for redirect endpoint

--- controllers/tests/links_test.go
+++ controllers/tests/links_test.go
@@ -19,14 +19,14 @@ func TestCreateLink(t *testing.T) {
 	w := performPostRequest(router, "/links", data)
 
 	// Assert we encoded correctly,
-	// the request gives a 200, w.Code
+	// the request gives a 201, w.Code
 	if w.Code != http.StatusCreated {
 		t.Errorf("TestConvertBase FAILED, expected value %v but got %v", http.StatusCreated, w.Code)
 	}
 }
 
 func TestGetLinks(t *testing.T) {
-	// Grab our router
+	// Grab our router and db connection
 	router, sqlite := initApp()
 	defer sqlite.Close()
 
@@ -49,7 +49,6 @@ func TestGetLinks(t *testing.T) {
 	err := json.Unmarshal([]byte(w.Body.String()), &response)
 
 	// Grab the value & whether or not it exists
-	// value, exists := response["hello"]
 	// Make some assertions on the correctness of the response.
 	if err != nil {
 		t.Errorf("TestConvertBase FAILED, expected value %v but got %v", nil, err)
@@ -64,5 +63,28 @@ func TestGetLinks(t *testing.T) {
 	returnedAddress := response["data"][0].Address
 	if returnedAddress != randomAddress {
 		t.Errorf("TestConvertBase FAILED, expected value %v but got %v", randomAddress, returnedAddress)
+	}
+}
+
+func TestRedirect(t *testing.T) {
+	// Grab our router
+	router, sqlite := initApp()
+	defer sqlite.Close()
+
+	randomAddress := "https://" + generateRandomString(8) + ".com"
+	data := map[string]string{"address": randomAddress}
+
+	w := performPostRequest(router, "/links", data)
+	// Convert the JSON response to a map
+	var response map[string]models.Link
+	json.Unmarshal([]byte(w.Body.String()), &response)
+
+	redirectAddress := response["data"].Shortcut
+	res := performGetRequest(router, redirectAddress)
+
+	// Assert we encoded correctly,
+	// the request gives a 200, w.Code)
+	if res.Code != http.StatusMovedPermanently {
+		t.Errorf("TestConvertBase FAILED, expected value %v but got %v", http.StatusMovedPermanently, res.Code)
 	}
 }
